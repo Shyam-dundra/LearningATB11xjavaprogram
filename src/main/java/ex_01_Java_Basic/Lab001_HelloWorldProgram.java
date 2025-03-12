2b946a9ecf5d0e7507cfbfaa0b680412ef6cc7dc
shyam-dundra <sunnydundra97@gmail.com>
2025-03-12 10:19:15 +0530

    12 - march task to print 9 table

---
+++ src/main/java/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -0,0 +1,22 @@
+package ex_01_Java_Basic;
+
+public class Lab001_HelloWorldProgram {
+    public static void main(String[] args) {
+//        System.out.println("Hello world am started learning core JAVA");
+
+//        star pattern program
+        int n = 6;
+//        for (int i = 1; i  <=5; i++) {
+//
+//            for (int j = 1; j <=i; j++) {
+//                System.out.print("*");
+//            }
+//
+//            System.out.println();
+//
+//        }
+
+
+
+    }
+}
