7ccb443eddb76d8718b0ccae7e294e1b95f2c012
Shyam-dundra <sunnydundra97@gmail.com>
2025-04-03 13:06:16 +0000

    polymorphism

---
+++ src/ex_06_Oops_polymorphism/MethodOverriding/ex_01_Oops_polymorphism.java
@@ -0,0 +1,23 @@
+package ex_06_Oops_polymorphism.MethodOverriding;
+
+public class ex_01_Oops_polymorphism {
+    public static void main(String[] args) {
+        Son s1 = new Son();
+        s1.home();
+        
+        father f1 = new father();
+        f1.home();
+    }
+
+}
+class father{
+    void home() {
+        System.out.println("BHK2");
+    }
+}
+class Son extends father{
+    @Override
+    void home() {
+        System.out.println("BHK3");
+    }
+}
