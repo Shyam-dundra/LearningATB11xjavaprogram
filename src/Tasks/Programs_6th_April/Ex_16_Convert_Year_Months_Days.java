4c01632bf2f5cad6821c01488dd4fb42cf147dcf
Shyam Dundra <sunnydundra97@gmail.com>
2025-04-09 22:46:56 +0530

    April 6th Task

--- src/Tasks/Programs_6th_April/Ex_16_Convert_Year_Months_Days.java
+++ src/Tasks/Programs_6th_April/Ex_16_Convert_Year_Months_Days.java
@@ -2,7 +2,7 @@ package Tasks.Programs_6th_April;
 
 import java.util.Scanner;
 
-public class Ex_16_Leap_Year {
+public class Ex_16_Convert_Year_Months_Days {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the days : ");
